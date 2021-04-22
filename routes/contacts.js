4c60ccfcaf1e2dab2c5a9aed5826d1f335ac5045
Tuan N. Truong <tuan.truong625@gmail.com>
2021-04-22 15:49:21 -0400

    Create contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,9 +24,36 @@ router.get('/', auth, async (req, res) => {
 // @route POST api/contacts
 // @description Add new contact
 // @access Private
-router.get('/', (req, res) => {
- res.send('Add contact')
-});
+router.post(
+ '/',
+ [auth, [
+  check('name', 'Name is required').not().isEmpty()
+ ],],
+ async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { name, email, phone, type } = req.body
+
+  try {
+   const newContact = new Contact({
+    name,
+    email,
+    phone,
+    type,
+    user: req.user.id
+   })
+
+   const contact = await newContact.save()
+
+   res.json(contact)
+  } catch (e) {
+   console.error(e.message)
+   res.status(500).send('Server Error')
+  }
+ });
 
 // @route PUT api/contacts/:id
 // @description Update contact
