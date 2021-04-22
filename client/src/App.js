c94e9759e89f21e8a0b11d43d7e55345fda5c4b3
Tuan N. Truong <tuan.truong625@gmail.com>
2021-04-22 19:49:40 -0400

    Create navbar/about/home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
 
-    </div>
+        </div>
+      </>
+    </Router>
   );
 }
 
